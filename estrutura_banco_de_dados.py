184e52dde817922072838c92cc435c47f1f55f30
marcelawn <cecelosarmento@gmail.com>
2024-04-01 20:29:14 -0300

    update2022

--- estrutura_banco_de_dados.py
+++ estrutura_banco_de_dados.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # Criar um instância de SQLAlchemy
 app.config['SECRET_KEY'] = 'FSD2323f#$!SAH'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres.flbpimpinfdemhjuvoqy:fortnite10jms@aws-0-us-west-1.pooler.supabase.com:5432/postgres'
 
 
 db = SQLAlchemy(app)
